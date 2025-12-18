7efe3692b15227db6b15dd4ab54f8e3042fb15d1
Aryan Kumar <aryan6533578@gmial.com>
2025-12-18 22:35:44 +0530

    datatype js

---
+++ FRONTEND/JAVASCRIPT/datatypedemo.js
@@ -0,0 +1,29 @@
+let productprize = 9900;
+let productdiscount= 15 ;
+let productname = "Apple Iphone 11 pro max";
+let isAvailable = "True"; // ""= string used
+
+let firstname = "aryan";
+console.log(firstname);
+
+console.log(isAvailable);
+
+let minuszero = -0;  // js me negative print hota h
+console.log(minuszero);
+
+let x;  // when only variable ko declare krkr chore so undefined dega
+console.log(x);
+let y = null;
+console.log(y);
+
+let z = 5452896631074855963333345122n; // "Bigint" use kiye last me 'n' dal ke so that big no print easily  
+console.log(z);
+
+let product = {   // "object" creation inside sara key h and uska value diye h 
+    name : "apple iphone 11 pro max",  // sara keyvalue ka end me comma se end hoga
+    price : 150000,
+    discount : 15,
+    isAvailable : true,
+};
+
+console.log(product);
