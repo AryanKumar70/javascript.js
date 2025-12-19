234124499c35ebe951e9628f3c36692361468995
Aryan Kumar <aryan6533578@gmial.com>
2025-12-19 22:28:13 +0530

    boolean operator added

--- FRONTEND/JAVASCRIPT/operators/assignmentOperator.js
+++ FRONTEND/JAVASCRIPT/operators/assignmentOperator.js
@@ -5,3 +5,17 @@ x *=5;
 console.log(x);
 x /= 5;
 console.log(x);
+
+// BOOLEAN operator 
+console.log(false && false);
+console.log(true && false);
+console.log(false && true);
+console.log(true && true);
+
+console.log(false || false);
+console.log(true || false);
+console.log(false || true);
+console.log(true || true);
+
+console.log(!true);
+console.log(!false);
