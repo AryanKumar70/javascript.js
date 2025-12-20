097047ef9da758bb26dd5f8b3bdf781158f1b6aa
Aryan Kumar <aryan6533578@gmial.com>
2025-12-20 22:20:23 +0530

    operator concept added

--- FRONTEND/JAVASCRIPT/operators/assignmentOperator.js
+++ FRONTEND/JAVASCRIPT/operators/assignmentOperator.js
@@ -7,8 +7,8 @@ x /= 5;
 console.log(x);
 
 // BOOLEAN operator 
-console.log(false && false);
-console.log(true && false);
+console.log(false && false); // AND operator me agar pahla false so overall false hoga 2nd wla check bhi nhi krta h js
+console.log(true && false); // AND operator me agar pahla true so 2nd wla result hi final result bnta h (true && false = false)
 console.log(false && true);
 console.log(true && true);
 
@@ -19,3 +19,8 @@ console.log(true || true);
 
 console.log(!true);
 console.log(!false);
+
+console.log(10 && 30);  // FALSY VALUE (0) = +0 , -0 ,null,undefined, "" etc.
+console.log(0 && 30);
+console.log(10 && null);
+console.log("" && -0);
