32eece3601ac364d3f960331663b4fa6b379614d
Aryan Kumar <aryan6533578@gmial.com>
2026-02-06 22:36:14 +0530

    scope phase wise

---
+++ FRONTEND/JAVASCRIPT/SCOPES AND FUNCTION/scope2.js
@@ -0,0 +1,13 @@
+var teacher = "Sanket singh";  // Phase-1 me scope define hota h sara formal variable(let,var,const) and function ka mtlb box bn jayega variable name se empty 
+function fun(){              // phase-2 me value declare hota h 
+    var teacher = "Sarthak";
+    console.log("hellow",teacher);
+
+    function gun(){
+        var student = "Aryan";
+        console.log(student,teacher);
+
+    }
+    gun();
+}
+fun();
