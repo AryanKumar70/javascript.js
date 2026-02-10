267d4cdc176a9389cb42a1c4d95d5dc940e01433
Aryan Kumar <aryan6533578@gmial.com>
2026-02-10 17:03:01 +0530

    scope more property

---
+++ FRONTEND/JAVASCRIPT/SCOPES AND FUNCTION/scope6.js
@@ -0,0 +1,14 @@
+// let x = 20;  // 'let' does not allow to redeclare the same variable so throw error in (phase -1 ) 
+// console.log(x);
+// let x = 20;
+// console.log(x);
+
+// var x = 20;  // here also  throw error in (phase -1 ) 
+// console.log(x);
+// let x = 20;
+// console.log(x);
+
+// var x = 20;  // here 'var' allow redeclaration 
+// console.log(x);
+// var x = 20;
+// console.log(x);
