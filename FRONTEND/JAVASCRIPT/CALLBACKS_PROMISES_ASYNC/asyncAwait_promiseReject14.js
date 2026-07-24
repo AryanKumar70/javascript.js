1b94a4b8dab7b7972279e69414d5e0e31484d33e
Aryan Kumar <aryan6533578@gmial.com>
2026-07-24 14:55:46 +0530

    async promise when reject

--- FRONTEND/JAVASCRIPT/CALLBACKS_PROMISES_ASYNC/asyncAwait_promiseReject14.js
+++ FRONTEND/JAVASCRIPT/CALLBACKS_PROMISES_ASYNC/asyncAwait_promiseReject14.js
@@ -17,7 +17,7 @@ function writeFile(data , fileName){
         setTimeout(() => {
             console.log("writing to a file" , fileName , "is done");
             let status = "success";
-            res(status);                 
+            rej(status);       // promise reject so "try catch" use hoga              
         } , 2000);
     });    
 
@@ -36,18 +36,25 @@ function upload(fileName ,url ){
 }
 
 async function exe(){  // 1.asyync is always return a promise object
-    console.log("start Execution: ");
+    try{  // rej  promise so execute kr rhe h problem hua so catch print something wrong 
+        console.log("start Execution: ");
 
-    const downloadData = await download("https://www.example.com"); //2. using "await"se immediately function ke bahar aayenge and promise resolve hoga then phir function me wapas jayenge for final resolve
-    console.log("Data Downloaded is " , downloadData ); // downloadData jb await hoga so promise bnayega jisme (status:pending , value:undefine, onful[] ,onrej[] )
+        const downloadData = await download("https://www.example.com"); //2. using "await"se immediately function ke bahar aayenge and promise resolve hoga then phir function me wapas jayenge for final resolve
+        console.log("Data Downloaded is " , downloadData ); // downloadData jb await hoga so promise bnayega jisme (status:pending , value:undefine, onful[] ,onrej[] )
 
-    const fileResponse = await writeFile(downloadData , "example.txt");
-    console.log("File write status " , fileResponse );
+        const fileResponse = await writeFile(downloadData , "example.txt");
+        console.log("File write status " , fileResponse );
 
-    const uploadStatus = await upload("example.txt" , "https://www.example.com");
-    console.log("Upload status " , uploadStatus );
+        const uploadStatus = await upload("example.txt" , "https://www.example.com");
+        console.log("Upload status " , uploadStatus );
 
-    return uploadStatus;
+        return uploadStatus;
+
+    } catch(e){
+        console.log("Something Went Wrong");
+    }
+
+   
 }
 
 exe().then((v) => console.log("exec done " , v));  //3. exe is asyn function so promise return krega and ".then" so callback "v" wla filled hoga onfulfillment array me 
